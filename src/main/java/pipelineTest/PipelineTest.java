c74db2603a0ebcc03009d5da7bb37fb8db754ad1
Abdeljalil Bounaime <abdeljalil.bounaime@atos.net>
2019-01-07 16:12:38 +0100

    firstcommit

---
+++ src/main/java/pipelineTest/PipelineTest.java
@@ -0,0 +1,10 @@
+package pipelineTest;
+
+public class PipelineTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Abdeljalil");	
+	}
+
+}
